39e5983e117b8214e7229ad61165479538c7aa3f
sunday <sundaysdw@gmail.com>
2020-03-29 04:53:51 +0800

    built

---
+++ code/build/precache-manifest.aaaefc8fb1ecd236967841471d97f96f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08bf7a003cbde0d68cc8c143ef56ae0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f307867e796b64cf5071",
+    "url": "/static/css/main.c6742df3.chunk.css"
+  },
+  {
+    "revision": "251ed07b0ec416dca317",
+    "url": "/static/js/2.395e48b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.395e48b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f307867e796b64cf5071",
+    "url": "/static/js/main.26b446cf.chunk.js"
+  },
+  {
+    "revision": "dc1f3b9c1b139cb39897",
+    "url": "/static/js/runtime-main.69e10390.js"
+  },
+  {
+    "revision": "b6688c657e0f46b21c8064f117a1ded8",
+    "url": "/static/media/arabic.b6688c65.svg"
+  },
+  {
+    "revision": "b4f94876736d346bccfad21bea36a2b3",
+    "url": "/static/media/asiamiles_coin.b4f94876.png"
+  },
+  {
+    "revision": "a14e4c3e86a408f2375fee46e91ea50e",
+    "url": "/static/media/bar_chart_empty.a14e4c3e.svg"
+  },
+  {
+    "revision": "c694237615db9382f8bee40b26141f24",
+    "url": "/static/media/bar_chart_full.c6942376.svg"
+  },
+  {
+    "revision": "9c682f335c0d770db5a8e9e93839a56b",
+    "url": "/static/media/bar_chart_half.9c682f33.svg"
+  },
+  {
+    "revision": "0c91b47cb250f56daf203d5fdb4486a9",
+    "url": "/static/media/convoy_logo.0c91b47c.png"
+  },
+  {
+    "revision": "9807bc310f37f461c9cf52c8a6746dbe",
+    "url": "/static/media/custom_modal.9807bc31.svg"
+  },
+  {
+    "revision": "e5738c0d8a5ca98df53431ee1c641400",
+    "url": "/static/media/elderly.e5738c0d.svg"
+  },
+  {
+    "revision": "ce3b1ce09a6c6d26d94aa9e841974f6c",
+    "url": "/static/media/elderly_skydive.ce3b1ce0.svg"
+  },
+  {
+    "revision": "329e7ffc8b4e849256d539f22d823100",
+    "url": "/static/media/elderly_travel.329e7ffc.svg"
+  },
+  {
+    "revision": "d0317285fb592d7dd68d18f40828d522",
+    "url": "/static/media/health_check.d0317285.svg"
+  },
+  {
+    "revision": "3253b44b98e4decfca8df35e683753ca",
+    "url": "/static/media/heart.3253b44b.svg"
+  },
+  {
+    "revision": "494c7a12d4a93b690635994b1d5533d3",
+    "url": "/static/media/home_bad.494c7a12.png"
+  },
+  {
+    "revision": "6cf0de645132bc42c63df72dc1e7f96b",
+    "url": "/static/media/home_good.6cf0de64.png"
+  },
+  {
+    "revision": "94f216c585e1f3b65533382f6992134f",
+    "url": "/static/media/indian_hat.94f216c5.svg"
+  },
+  {
+    "revision": "63dec2444544b3b8eadc80c1f7e3f2fd",
+    "url": "/static/media/jp_guy.63dec244.svg"
+  },
+  {
+    "revision": "a0979b175267cfe8446317adb72e8311",
+    "url": "/static/media/knitting.a0979b17.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69b7054fb2262e0eb183d3639bf42125",
+    "url": "/static/media/money.69b7054f.svg"
+  },
+  {
+    "revision": "dc7ea403aa1c3077019d32d231b0536c",
+    "url": "/static/media/parachute.dc7ea403.svg"
+  },
+  {
+    "revision": "e5bc7383baf7523d4410285e7078d717",
+    "url": "/static/media/skydive.e5bc7383.png"
+  },
+  {
+    "revision": "4f69f0288635342be796965fc74ce316",
+    "url": "/static/media/travel.4f69f028.png"
+  }
+]);
